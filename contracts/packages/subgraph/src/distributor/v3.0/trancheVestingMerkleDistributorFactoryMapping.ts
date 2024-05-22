ab83b75177e75912ec130fd736390a4de12e113e
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-05-22 22:13:24 +0300

    Delete scope directories

--- contracts/packages/subgraph/src/distributor/v3.0/trancheVestingMerkleDistributorFactoryMapping.ts
+++
@@ -1,9 +0,0 @@
-import { DistributorDeployed } from '../../../generated/TrancheVestingMerkleDistributorFactory/TrancheVestingMerkleDistributorFactory'
-import { getOrCreateAdvancedDistributor, getOrCreateTrancheVesting, getOrCreateMerkleSet } from '../../lib'
-
-export function handleDistributorDeployed(event: DistributorDeployed): void {
-  const distributorAddress = event.params.distributor.toHexString()
-  getOrCreateAdvancedDistributor(distributorAddress, event.block)
-  getOrCreateTrancheVesting(distributorAddress, event.block)
-  getOrCreateMerkleSet(distributorAddress, event.block)
-}
